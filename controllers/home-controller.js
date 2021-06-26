f36ebb658fce823e9a7775b11bad37fafa4ade2a
Hy Nguyen <hynguyen2727@gmail.com>
2021-06-26 09:18:01 +0700

    finished lab 02

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,3 +1,8 @@
-exports.get = function (request, response) {
-    response.render('home')
+var session = require('./session')
+exports.get = function (request, response, webconfig, model) {
+    var logged = session.logged(request)
+    response.render('home', {
+        root: '',
+        logged: logged
+    })
 }
--- main.js
+++ main.js
@@ -1,22 +1,38 @@
+var datasource = require('./datasource/simple-datasource');
+var model = require('./model/model.js').create(datasource)
+
 // Setup web app
 
 var exprees = require('express')
 var app = exprees()
 
 app.set('view engine', 'ejs')
+app.use(require('cookie-parser')())
 
 var router = exprees.Router()
-
 router.use(exprees.static('public'))
-
-var webconfig = require('./webconfig')
+var webconfig = require('./webconfig');
+var urlencodedParser = require('body-parser').urlencoded({
+    extended: false
+})
 
 function controller(name) {
     return require('./controllers/' + name + '-controller')
 }
 
 router.get('/', function (request, response) {
-    controller('home').get(request, response)
+    controller('home').get(request, response,webconfig,model)
+})
+
+router.get('/login', function (request, response) {
+    controller('login').get(request, response, webconfig, model)
+})
+
+router.post('/login', urlencodedParser, function (request, response) {
+    controller('login').post(request, response, webconfig, model)
+})
+router.get('/logout', function (request, response) {
+    controller('logout').get(request,response,webconfig)
 })
 app.use(webconfig.root, router)
 
